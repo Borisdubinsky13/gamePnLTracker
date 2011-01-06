bcbf953a1e72aab46ac8a61a151c9ce97964c64b
boris <dubi13@hotmail.com>
2011-01-06 01:54:15 -0600

    Final version for Beta (0.2)

--- src/com/gamePnLTracker/AboutHandler.java
+++ src/com/gamePnLTracker/AboutHandler.java
@@ -7,7 +7,13 @@ import com.admob.android.ads.AdView;
 import com.gamePnLTracker.R;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * @author Boris
@@ -15,6 +21,9 @@ import android.os.Bundle;
  */
 public class AboutHandler extends Activity
 {
+	public String TAG="gamePnLTracker";
+	public String SubTag="AboutHandler: ";
+	
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
@@ -32,5 +41,14 @@ public class AboutHandler extends Activity
 */
         AdView	adView = (AdView)findViewById(R.id.adAbout);
         adView.requestFreshAd();
+        
+        final Button deleteB = (Button)findViewById(R.id.dispRelNotes);
+        deleteB.setOnClickListener(new View.OnClickListener()
+        {
+        	public void onClick(View v) 
+        	{
+        		setContentView(R.layout.releasenotes);
+        	}
+	     });
  	}
 }
